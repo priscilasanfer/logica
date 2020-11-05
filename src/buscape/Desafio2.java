8fa249e541fe054894f96d11c493faa58b04ca35
Priscila Ferreira <priscilasanfer@gmail.com>
2020-11-05 09:31:47 -0300

    refatorando desafio de verificacao de sintaxe

--- src/buscape/Desafio2.java
+++ src/buscape/Desafio2.java
@@ -7,8 +7,8 @@ public class Desafio2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Stack<String> pilhaDeCaracterDeAbertura = new Stack<>();
-        Stack<String> pilhaDeCaracterFechamento = new Stack<>();
+        Stack<String> pilha = new Stack<>();
+        int erro = 0;
 
         System.out.println("Digite o texto desejado para que seja analisado:");
 
@@ -17,41 +17,34 @@ public class Desafio2 {
 
         for (int i = 0; i < caracter.length; i++) {
             if (caracter[i].equals("(") || caracter[i].equals("{") || caracter[i].equals("[")) {
-                pilhaDeCaracterDeAbertura.push(caracter[i]);
+                pilha.push(caracter[i]);
             }
 
-            if(caracter[i].equals(")")) {
-                if (!pilhaDeCaracterDeAbertura.isEmpty() && pilhaDeCaracterDeAbertura.peek().equals("(")){
-                    pilhaDeCaracterDeAbertura.pop();
-                } else{
-                   pilhaDeCaracterFechamento.push(caracter[i]);
+            if(pilha.isEmpty()) {
+                if (caracter[i].equals("}") || caracter[i].equals(")") || caracter[i].equals("]")) {
+                    erro++;
                     break;
                 }
             }
 
-            if(caracter[i].equals("]")) {
-                if (!pilhaDeCaracterDeAbertura.isEmpty() && pilhaDeCaracterDeAbertura.peek().equals("[")){
-                    pilhaDeCaracterDeAbertura.pop();
-                }else{
-                    pilhaDeCaracterFechamento.push(caracter[i]);
-                    break;
-                }
+            if (caracter[i].equals("}") && pilha.peek().equals("{")){
+                pilha.pop();
             }
 
-            if(caracter[i].equals("}")) {
-                if (!pilhaDeCaracterDeAbertura.isEmpty() && pilhaDeCaracterDeAbertura.peek().equals("{")){
-                    pilhaDeCaracterDeAbertura.pop();
-                }else{
-                    pilhaDeCaracterFechamento.push(caracter[i]);
-                    break;
-                }
+            if (caracter[i].equals(")") && pilha.peek().equals("(")){
+                pilha.pop();
+            }
+
+            if (caracter[i].equals("]") && pilha.peek().equals("[")){
+                pilha.pop();
             }
         }
 
-        if (pilhaDeCaracterDeAbertura.isEmpty() && pilhaDeCaracterFechamento.isEmpty()){
-            System.out.println("A sintaxe está OK");
-        }else {
-            System.out.println("A sintaxe não está OK");
+        if(pilha.isEmpty() && erro == 0){
+            System.out.println("A Sintaxe está ok");
+        }
+        else {
+            System.out.println("A Sintaxe não está ok");
         }
 
         sc.close();
