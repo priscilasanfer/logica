27090e9e145a715cd8fe28ce89f9de8b68199cba
Priscila Ferreira <priscilasanfer@gmail.com>
2020-11-16 06:56:45 -0300

    hackerrank

---
+++ src/hackerrank/thirtyDaysOfCode/ConditionalStatements.java
@@ -0,0 +1,22 @@
+package hackerrank.thirtyDaysOfCode;
+
+import java.util.Scanner;
+
+public class ConditionalStatements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (N % 2 != 0) {
+            System.out.println("Weird");
+        } else if (N >= 2 && N <= 5) {
+            System.out.println("Not Weird");
+        } else if (N >= 6 && N <= 20) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+    }
+}
